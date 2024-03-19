776c29629585a969bed6657433545968994af0b0
CodeHare <coder_bqr@163.com>
2024-03-19 16:47:57 +0800

    feat: 添加消防SESS、液冷-消防

---
+++ src/pages/config/LiquidCooledFireProtection/index.tsx
@@ -0,0 +1,51 @@
+/*
+ * @Description:
+ * @Author: YangJianFei
+ * @Date: 2024-03-15 16:40:51
+ * @LastEditTime: 2024-03-16 11:29:44
+ * @LastEditors: YangJianFei
+ * @FilePath: \ems-x-web\src\pages\state\pcs\index.tsx
+ */
+
+import React from 'react';
+import Card from '@/components/Card';
+import { Col, Row } from 'antd';
+import Run from '@/components/Device/Run';
+import { useModel, useRequest } from 'umi';
+import { getDeviceData } from '@/services/device';
+import { baseInfoItems, statusItems } from './helper';
+
+const LiquidCooledFireProtection: React.FC = () => {
+  const { config } = useModel('config');
+  const { data: realTimeData, run } = useRequest(getDeviceData, {
+    manual: true,
+    pollingInterval: config.refreshTime * 1000,
+  });
+
+  return (
+    <>
+      <div className="p24">
+        <Card className="h-full">
+          <Run
+            realTimeData={realTimeData}
+            groupData={baseInfoItems}
+            detailProps={{
+              column: 5,
+            }}
+          />
+        </Card>
+        <Card className="h-full mt20">
+          <Run
+            realTimeData={realTimeData}
+            groupData={statusItems}
+            detailProps={{
+              column: 5,
+            }}
+          />
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default LiquidCooledFireProtection;
