e4fae0ca9a3c75546e24595be7143c9cf8e30416
aoshilin <1602893986@qq.com>
2024-03-22 17:17:46 +0800

    feat: 底部状态栏处理

--- src/pages/index/IndustryStorage/index.tsx
+++ src/pages/index/IndustryStorage/index.tsx
@@ -12,29 +12,27 @@ import {
   systemItems,
 } from './helper';
 import Chart from '../Chart';
-import { useDeviceData } from '@/hooks';
+import RefreshData from '@/components/Device/RefreshData';
 import SystemRunStatus from '../SystemRunStatus';
 import batImg from '@/assets/image/station/overview/icon_储能电池.svg';
 import batSystemImg from '@/assets/image/station/overview/icon_储能.svg';
 import loadImg from '@/assets/image/station/overview/icon_负载1.svg';
 import incomeImg from '@/assets/image/station/overview/icon_收益.svg';
 import reduceImg from '@/assets/image/station/overview/icon_减排.svg';
-import SystemAlarm from "@/components/SystemAlarm";
-import {useDeviceData} from "@/hooks";
-
+import { useDeviceData } from '@/hooks';
+import { useModel } from 'umi';
 const Index: React.FC = () => {
   const { config } = useModel('config');
   const { realTimeData, run } = useDeviceData({ isInterval: false });
-
   return (
     <>
       <RefreshData run={run} time={realTimeData?.refreshTime} />
       <div className={styles.industry}>
         <Row gutter={12}>
           <Col span={8}>
-            <Card className={styles.card} style={{height: 198}}>
+            <Card className={styles.card} style={{ height: 198 }}>
               <div className={styles.icon}>
-                <img src={batImg} alt=""/>
+                <img src={batImg} alt="" />
                 <span>储能电池</span>
               </div>
               <Run
@@ -47,9 +45,9 @@ const Index: React.FC = () => {
             </Card>
           </Col>
           <Col span={8}>
-            <Card className={styles.card} style={{height: 198}}>
+            <Card className={styles.card} style={{ height: 198 }}>
               <div className={styles.icon}>
-                <img src={batSystemImg} alt=""/>
+                <img src={batSystemImg} alt="" />
                 <span>储能系统</span>
               </div>
               <Run
@@ -62,9 +60,9 @@ const Index: React.FC = () => {
             </Card>
           </Col>
           <Col span={8}>
-            <Card className={styles.card} style={{height: 198}}>
+            <Card className={styles.card} style={{ height: 198 }}>
               <div className={styles.icon}>
-                <img src={loadImg} alt=""/>
+                <img src={loadImg} alt="" />
                 <span>负载</span>
               </div>
               <Run
@@ -79,20 +77,20 @@ const Index: React.FC = () => {
         </Row>
         <Row gutter={12} className="mt16" align="top">
           <Col span={8}>
-            <Card style={{height: 412}} className={styles.card}>
+            <Card style={{ height: 412 }} className={styles.card}>
               <SystemRunStatus data={realTimeData} />
             </Card>
           </Col>
           <Col span={10}>
-            <Card style={{height: 412}} className={styles.card}>
+            <Card style={{ height: 412 }} className={styles.card}>
               <Chart run={run} />
             </Card>
           </Col>
           <Col span={6}>
             <div className={styles.right}>
-              <Card style={{height: 198}} className={styles.card}>
+              <Card style={{ height: 198 }} className={styles.card}>
                 <div className={styles.icon}>
-                  <img src={incomeImg} alt=""/>
+                  <img src={incomeImg} alt="" />
                   <span>收益</span>
                 </div>
                 <Run
@@ -103,9 +101,9 @@ const Index: React.FC = () => {
                   }}
                 />
               </Card>
-              <Card style={{height: 198}} className={styles.card}>
+              <Card style={{ height: 198 }} className={styles.card}>
                 <div className={styles.icon}>
-                  <img src={reduceImg} alt=""/>
+                  <img src={reduceImg} alt="" />
                   <span>减排</span>
                 </div>
                 <Run
@@ -133,7 +131,7 @@ const Index: React.FC = () => {
           </Col>
         </Row>
         <div className="mt16">
-          <SystemAlarm modelType="system" realTimeData={realTimeData}/>
+          <SystemAlarm modelType="system" realTimeData={realTimeData} />
         </div>
       </div>
     </>
