0ecd5f8cda75d87863daa35ef15d561aae4f7c7e
CodeHare <coder_bqr@163.com>
2024-03-27 16:33:28 +0800

    feat: 提交帮助页面 UI

--- src/pages/help/Help/index.tsx
+++ src/pages/help/Help/index.tsx
@@ -7,17 +7,23 @@
  * @FilePath: \ems-x-web\src\pages\help\Help\index.tsx
  */
 
-import React from 'react';
-import { useDeviceData } from '@/hooks';
-import RefreshData from '@/components/Device/RefreshData';
+import React, { useState } from 'react';
+import styles from './index.less'
+import { Button, } from 'antd';
+import { DownloadOutlined } from '@ant-design/icons';
+// import { ConfigProviderProps } from 'antd/lib/config-provider';
+// type SizeType = ConfigProviderProps['componentSize'];
 
 const Help: React.FC = () => {
-  const { realTimeData, run } = useDeviceData();
+  // const [size] = useState<SizeType>('large');
 
   return (
     <>
-      <RefreshData run={run} time={realTimeData?.refreshTime} />
-      <div className="p24"></div>
+      <div className={styles.title}>YT-ESS-EMS工商储能量管理系统上位机软件&nbsp;V1.0</div>
+      <div className={styles.subTitle}>适配&nbsp;EMSV1.0&nbsp;通信盒</div>
+      <div className={styles.download}>
+        {/* <Button type="primary" icon={<DownloadOutlined />} size={size}>Download</Button> */}
+      </div>
     </>
   );
 };
